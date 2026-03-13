0ea38568b238b1cf7723eb37dd251efdbf315aaa
Liam-Sango <anonymous_aussie42@proton.me>
2026-03-13 11:56:58 +1100

    Worked on Random data generation for use in the entire program

--- include/Modules/random.h
+++ include/Modules/random.h
@@ -1,4 +1,10 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+//Generates securely random raw bytes. (Without any conversion or processing)
+int Generate_raw_random_bytes(int Gen_amt, unsigned char rand_buffer[Gen_amt]);
+
+//generates an array of random numbers for use in credential generation (Unfinished)
+int Generate_random_numbers(int Gen_amt, unsigned char rand_buffer[Gen_amt]);
+
 #endif // RANDOM_H
--- src/Modules/random.c
+++ src/Modules/random.c
@@ -6,3 +6,36 @@
 #include <openssl/err.h>
 
 #include "../../include/Modules/random.h" 
+
+int Generate_raw_random_bytes(int Gen_amt, unsigned char rand_buffer[Gen_amt]) {
+    //Ensures cryptographic algorithms and error strings are available.
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    // Check if RNG is properly seeded
+    if (RAND_poll() != 1) {
+        unsigned char seed[16] = {0};  // Using zeroed seed as fallback
+        RAND_seed(seed, sizeof(seed));
+    }
+
+    //Generates random bytes
+    int result = RAND_bytes(rand_buffer, Gen_amt);
+    return 0;
+};
+
+//Unfinished
+int Generate_random_numbers(int Gen_amt, unsigned char rand_buffer[Gen_amt]) {
+    //Ensures cryptographic algorithms and error strings are available.
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    // Check if RNG is properly seeded
+    if (RAND_poll() != 1) {
+        unsigned char seed[16] = {0};  // Using zeroed seed as fallback
+        RAND_seed(seed, sizeof(seed));
+    }
+
+    //Generates random bytes
+    int result = RAND_bytes(rand_buffer, Gen_amt);
+    return 0;
+};
